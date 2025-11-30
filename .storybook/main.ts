152430474f81562d6ace58faa40523ea123a5a67
Nick-the-Descended <mujirishvilinika@gmail.com>
2025-12-01 00:54:29 +0400

    UPDATE: Remove `package-lock.json` file to discontinue its usage

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -8,33 +8,7 @@ const config: StorybookConfig = {
         '@storybook/addon-vitest',
         '@storybook/addon-a11y',
         '@storybook/addon-docs',
-        {
-            name: '@storybook/addon-styling-webpack',
-            options: {
-                rules: [
-                    {
-                        test: /\.css$/,
-                        sideEffects: true,
-                        use: [
-                            require.resolve('style-loader'),
-                            {
-                                loader: require.resolve('css-loader'),
-                                options: {
-                                    importLoaders: 1
-                                }
-                            },
-                            {
-                                loader: require.resolve('postcss-loader'),
-                                options: {
-                                    implementation:
-                                        require.resolve('postcss')
-                                }
-                            }
-                        ]
-                    }
-                ]
-            }
-        }
+        '@storybook/addon-styling-webpack'
     ],
     framework: '@storybook/sveltekit'
 };
